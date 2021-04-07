a76dc57ec2ec1599f89623ebb6f7bcbd299e1dda
Tushar Sahu <tusharshahu77@gmail.com>
2021-04-07 12:45:11 +0530

    update cart utils to group items in cart reducer

--- src/components/CollectionItems/collection-item.component.jsx
+++ src/components/CollectionItems/collection-item.component.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
-import './collection-item.styles.scss';
+import { connect } from 'react-redux';
 
-const CollectionItem = ({ name, price, imageUrl }) => {
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
+
+import './collection-item.styles.scss';
+//the second argument(addItem) in the CollectionItem is the function which we made in the mapDispatchToProps
+
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+
     return (
         <div className='collection-item'>
-            <div className='image' style={{
-                backgroundImage: `url(${imageUrl})`
-            }} />
+            <div
+                className='image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
             <div className='collection-footer'>
                 <span className='name'>{name}</span>
-                <span className='price'>{`$${price}`}</span>
+                <span className='price'>{price}</span>
             </div>
+            <CustomButton onClick={() => addItem(item)} inverted>
+                Add to cart
+      </CustomButton>
         </div>
-    )
-}
+    );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(
+    //here we are making addItem fuction which takes item as argument which calls the addItem fuction from cart.action
+    //the addItem fuction inside dipatch is the function that we import from cart.action
+    null,
+    mapDispatchToProps
+)(CollectionItem);
+
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
-import { toggleCartHidden } from '../../redux/cart/cart.action';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { connect } from 'react-redux'
 
+//the second argument(toggleCartHidden) in the CartIcon is the function which we made in the mapDispatchToProps
 const CartIcon = ({ toggleCartHidden }) => {
     return (
         <div className='cart-icon' onClick={toggleCartHidden}>
@@ -12,7 +13,8 @@ const CartIcon = ({ toggleCartHidden }) => {
         </div>
     )
 }
-
+//here we are making toggleCartHidden fuction which calls the toggleCartHidden fuction from cart.action
+//the toggleCartHidden fuction inside dipatch is the function that we import from cart.action
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
