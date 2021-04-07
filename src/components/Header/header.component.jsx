c81c328c07fb61ce7a8af4088fa0911faef10077
Tushar Sahu <tusharshahu77@gmail.com>
2021-04-07 10:29:08 +0530

    added cartIcon and cartDropdown components to our application, also implemented cart reducer

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 import { connect } from 'react-redux';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return (
         <div className='header'>
             <Link className='logo-container' to='/'>
@@ -18,17 +20,22 @@ const Header = ({ currentUser }) => {
                 <Link className='option' to='/contact'>CONTACT</Link>
                 {
                     currentUser ?
-                        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                        (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
                         :
-                        <Link className='option' to='/signin'>SIGN IN</Link>
+                        (<Link className='option' to='/signin'>SIGN IN</Link>)
                 }
+                <CartIcon />
             </div>
+            {
+                hidden ? null : <CartDropdown />
+            }
         </div>
     )
 }
 //it is a function which assign prop(currentUser) to the component(Header) which takes the value of currentUser from the rootReducer
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden,
 })
 //connect is a higher order component which take two arguments
 export default connect(mapStateToProps)(Header);
