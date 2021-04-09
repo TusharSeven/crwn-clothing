e184f51d80487828426f5fb717abb8a936abea4b
Tushar Sahu <tusharshahu77@gmail.com>
2021-04-09 10:19:23 +0530

    modified file names accordinng to conventions

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,52 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { createStructuredSelector } from 'reselect';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
-import './header.styles.scss';
-
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-import { connect } from 'react-redux';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-const Header = ({ currentUser, hidden }) => {
-    return (
-        <div className='header'>
-            <Link className='logo-container' to='/'>
-                <Logo className='logo' />
-            </Link>
-            <div className='options'>
-                <Link className='option' to='/shop'>SHOP</Link>
-                <Link className='option' to='/contact'>CONTACT</Link>
-                {
-                    currentUser ?
-                        (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
-                        :
-                        (<Link className='option' to='/signin'>SIGN IN</Link>)
-                }
-                <CartIcon />
-            </div>
-            {
-                hidden ? null : <CartDropdown />
-            }
+import './header.styles.scss';
+
+const Header = ({ currentUser, hidden }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
-    )
-}
-//it is a function which assign prop(currentUser) to the component(Header) which takes the value of currentUser from the rootReducer
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
+      <CartIcon />
+    </div>
+    {hidden ? null : <CartDropdown />}
+  </div>
+);
 
-// const mapStateToProps = (state) => ({
-//     currentUser: selectCurrentUser(state),
-//     hidden: selectCartHidden(state)
-// })
-
-//it gets repeated to write state in arhgument in the above method , thats why we use createStructuredSelector
-//another way
 const mapStateToProps = createStructuredSelector({
-    currentUser: selectCurrentUser,
-    hidden: selectCartHidden
-})
-//connect is a higher order component which take two arguments
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
+});
+
 export default connect(mapStateToProps)(Header);
