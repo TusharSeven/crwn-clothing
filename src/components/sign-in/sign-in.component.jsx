f6c65f4bd76c19f6346a8ababae2976ea7cc1633
Tushar Sahu <tusharshahu77@gmail.com>
2021-04-12 19:09:16 +0530

    update credit card details

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,27 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
 import { auth, SignInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
-class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
+const SignIn = () => {
 
-        this.state = {
-            email: '',
-            password: ''
-        };
+    const [userCredentials, setUserCredentials] = useState({ email: '', password: '' });
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    const { email, password } = userCredentials;
 
-
-    async handleSubmit(event) {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        const { email, password } = this.state;
 
         try {
             await auth.signInWithEmailAndPassword(email, password);
@@ -31,40 +22,38 @@ class SignIn extends React.Component {
         }
     };
 
-    handleChange(event) {
+    const handleChange = event => {
         const { value, name } = event.target;
 
-        this.setState({ [name]: value });
+        setUserCredentials({ ...userCredentials, [name]: value });
     };
 
 
-    render() {
-        return (
-            <div className='sign-in'>
-                <h2 className='title'>I already have an account</h2>
-                <span>Sign in with your email and password</span>
+    return (
+        <div className='sign-in'>
+            <h2 className='title'>I already have an account</h2>
+            <span>Sign in with your email and password</span>
 
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput name='email'
-                        type='email'
-                        handleChange={this.handleChange}
-                        value={this.state.email}
-                        label='Email'
-                        required />
-                    <FormInput name='password'
-                        type='password'
-                        value={this.state.password}
-                        handleChange={this.handleChange}
-                        label='Password'
-                        required />
-                    <div className='buttons'>
-                        <CustomButton type="submit">Sign in</ CustomButton>
-                        <CustomButton type='button' onClick={SignInWithGoogle} isGoogleSignIn>Sign in with google</ CustomButton>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+            <form onSubmit={handleSubmit}>
+                <FormInput name='email'
+                    type='email'
+                    handleChange={handleChange}
+                    value={email}
+                    label='Email'
+                    required />
+                <FormInput name='password'
+                    type='password'
+                    value={password}
+                    handleChange={handleChange}
+                    label='Password'
+                    required />
+                <div className='buttons'>
+                    <CustomButton type="submit">Sign in</ CustomButton>
+                    <CustomButton type='button' onClick={SignInWithGoogle} isGoogleSignIn>Sign in with google</ CustomButton>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default SignIn;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,17 +3,23 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 //for logging out states to the console
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
+import reduxSaga from 'redux-saga';
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// sagaMiddleware.run();
 
 //creating a persisted version of store
 const persistor = persistStore(store);
